1d0af540dcb79161f33e32856ef95887d2af50ae
AKim47 <abel.kim.22@gmail.com>
2022-02-16 22:25:34 -0600

    service worker fix

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,6 @@
+const APP_PREFIX = 'PWABudgetTracker-';
+const VERSION = 'version_01';
+const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
     "./public/index.html",
     "./public/css/styles.css",
@@ -5,14 +8,25 @@ const FILES_TO_CACHE = [
     "./public/js/index.js"
 ];
 
-const APP_PREFIX = 'PWABudgetTracker-';
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
+self.addEventListener('fetch', function (e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        caches.match(e.request).then(function (request) {
+            if (request) {
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else {
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch.apply(e.request)
+            }
+        })
+    )
+})
 
 self.addEventListener('install', function (e) {
     e.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
-            console.log('installing cache: ' + CACHE_NAME)
+            console.log('installing cache : ' + CACHE_NAME)
             return cache.addAll(FILES_TO_CACHE)
         })
     )
@@ -37,18 +51,3 @@ self.addEventListener('activate', function (e) {
         })
     );
 });
-
-self.addEventListener('fetch', function (e) {
-    console.log('fetch request : ' + e.request.url)
-    e.respondWith(
-        caches.match(e.request).then(function (request) {
-            if (request) {
-                console.log('responding with cache : ' + e.request.url)
-                return request
-            } else {
-                console.log('file is not cached, fetching : ' + e.request.url)
-                return fetch.apply(e.request)
-            }
-        })
-    )
-})
